481828619b4afcb6a0f84ad695452ce1c6e25dcd
Oleksandr Humeniuk <merokyry33@gmail.com>
2024-07-24 16:33:00 +0200

    task2

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,12 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  const newContacts = Array.from({ length: number }, createFakeContact);
+  const existData = await fs.readFile(PATH_DB, 'utf-8');
+  const newData = [...JSON.parse(existData), ...newContacts];
+  await fs.writeFile(PATH_DB, JSON.stringify(newData, null, 2));
+};
 
-generateContacts(5);
+generateContacts(2);
