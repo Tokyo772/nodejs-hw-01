fb5696d182044a7e14d63262dcdb894a72974f0c
Oleksandr Humeniuk <merokyry33@gmail.com>
2024-07-24 17:03:50 +0200

    task5

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,9 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+  const existData = await fs.readFile(PATH_DB, 'utf-8');
+  return JSON.parse(existData).length;
+};
 
 console.log(await countContacts());
